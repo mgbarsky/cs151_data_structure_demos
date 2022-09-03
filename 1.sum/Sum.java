a0f0b78d9fab2ed9347c6265930c85fc3f1c485b
Marina Barsky <mbarsky@oberlin.edu>
2022-09-02 22:55:26 -0400

    sum

---
+++ 1.sum/Sum.java
@@ -0,0 +1,17 @@
+/*
+ * Sum.java
+ * Author: CS 151 staff
+ * Fall 2022
+ * This program sums two numbers provided as program arguments
+ * and prints the results  to the terminal.
+ */
+public class Sum {
+
+  	public static void main(String[] args) {
+			int s;
+    		int a = Integer.valueOf(args[0]);
+			int b = Integer.valueOf(args[1]);
+			s = a + b;
+			System.out.println(a + " + " + b + " = " + s);
+  	}
+}
